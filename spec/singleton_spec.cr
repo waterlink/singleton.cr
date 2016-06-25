7333fb0f294d1f45fa7b055c6ff26d2d5233dfbf
Oleksii Fedorov <waterlink000@gmail.com>
2016-06-25 02:31:06 +0200

    Implement simple singleton

--- spec/singleton_spec.cr
+++ spec/singleton_spec.cr
@@ -1,9 +1,56 @@
 require "./spec_helper"
 
-describe Singleton do
-  # TODO: Write tests
+class Example
+end
 
-  it "works" do
-    false.should eq(true)
+class OtherExample
+end
+
+class StatefulExample
+  getter value : Int32
+  @value = 42
+
+  def inc
+    @value += 1
+  end
+end
+
+Spec.before_each do
+  Singleton.reset
+end
+
+describe Singleton do
+  describe ".instance" do
+    it "can be fetched" do
+      example = Singleton.instance_of(Example)
+      example.should be_a(Example)
+      typeof(example).should eq(Example)
+    end
+
+    it "is the same when fetched again" do
+      example = Singleton.instance_of(Example)
+      again_example = Singleton.instance_of(Example)
+      example.should be(again_example)
+      typeof(again_example).should eq(Example)
+    end
+
+    it "is of correct type when other type" do
+      other_example = Singleton.instance_of(OtherExample)
+      other_example.should be_a(OtherExample)
+      typeof(other_example).should eq(OtherExample)
+    end
+  end
+
+  describe ".reset" do
+    it "resets all instances correctly" do
+      stateful_example = Singleton.instance_of(StatefulExample)
+      stateful_example.inc
+      stateful_example.value.should eq(43)
+
+      Singleton.reset
+      
+      stateful_example = Singleton.instance_of(StatefulExample)
+      stateful_example.value.should eq(42)
+    end
   end
 end
--- src/singleton.cr
+++ src/singleton.cr
@@ -1,5 +1,24 @@
-require "./singleton/*"
+class Singleton
+  abstract class Producer
+    abstract def produce
+  end
 
-module Singleton
-  # TODO Put your code here
+  class GenericProducer(T) < Producer
+    getter produce : T
+    def initialize(@produce) end
+  end
+
+  @@instances = {} of String => Producer
+
+  def self.reset
+    @@instances.clear
+  end
+
+  def self.instance_of(t : T.class) : T
+    unless @@instances.has_key?(t.name)
+      @@instances[t.name] = GenericProducer(T).new(t.new)
+    end
+
+    @@instances[t.name].produce as T
+  end
 end
